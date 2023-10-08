cbd6eaa9dfaba50ad691545f90bfa17107816438
Futa HIRAKOBA <korosuke613613@gmail.com>
2023-10-08 20:33:17 +0900

    feat: greet!

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput("milliseconds");
 
+    core.info("Hello world!");
+
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`);
 
